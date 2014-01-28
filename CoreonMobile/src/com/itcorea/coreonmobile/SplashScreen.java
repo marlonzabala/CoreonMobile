b0c15fb463b100428336ca1d76188a029c1fe91d
Marlon <marlonzbl@gmail.com>
2014-01-28 16:43:22 +0800

    added splash screen

---
+++ CoreonMobile/src/com/itcorea/coreonmobile/SplashScreen.java
@@ -0,0 +1,63 @@
+package com.itcorea.coreonmobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class SplashScreen extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_splash);
+
+		new LogInExecute().execute();
+	}
+
+	private class LogInExecute extends AsyncTask<String, Void, String>
+	{
+		@Override
+		protected void onPreExecute()
+		{
+
+		}
+
+		@Override
+		protected String doInBackground(String... params)
+		{
+			try
+			{
+				Thread.sleep(2000);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+
+			return "";
+		}
+
+		@Override
+		protected void onPostExecute(String result)
+		{
+			Intent newIntent = new Intent(SplashScreen.this, LogIn.class);
+			startActivity(newIntent);
+			((Activity) SplashScreen.this).finish();
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values)
+		{
+
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		return true;
+	}
+}
